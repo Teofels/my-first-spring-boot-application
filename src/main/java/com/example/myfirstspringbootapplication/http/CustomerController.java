371b90470e0286bb7ec7501c62d1172e77935aed
Dmitry <dimastr61@gmail.com>
2022-06-03 23:02:29 +0300

    добавил новый endpoint : /customer/{id} + обработка исключения для этого эндпоинта

--- src/main/java/com/example/myfirstspringbootapplication/http/CustomerController.java
+++ src/main/java/com/example/myfirstspringbootapplication/http/CustomerController.java
@@ -1,25 +1,28 @@
 package com.example.myfirstspringbootapplication.http;
 
 import com.example.myfirstspringbootapplication.dto.Customer;
+import com.example.myfirstspringbootapplication.exceptions.CustomerNotFoundException;
 import com.example.myfirstspringbootapplication.service.CustomerRepository;
-import com.example.myfirstspringbootapplication.service.CustomerService;
 import lombok.AllArgsConstructor;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 @RestController
 @AllArgsConstructor
 public class CustomerController {
     private final CustomerRepository repository;
 
-
     /**
      * пример BODY для отправки: {"firstName": "testFirstName", "lastName": "testLastName"}
      */
     @PostMapping(path = "/customer/registration")
-    public Customer getUser(@RequestBody Customer customer) {
+    public Customer register(@RequestBody Customer customer) {
         return repository.save(customer);
     }
 
+    @GetMapping("/customer/{id}")
+    Customer getCustomerById(@PathVariable Long id) {
+
+        return repository.findById(id)
+                .orElseThrow(() -> new CustomerNotFoundException(id));
+    }
 }
--- src/main/java/com/example/myfirstspringbootapplication/service/CustomerService.java
+++
@@ -1,4 +0,0 @@
-package com.example.myfirstspringbootapplication.service;
-
-public interface CustomerService {
-}
