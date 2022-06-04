89c297e06943ad7b6120bbe42ca4efd443fa8ca0
Dmitry <dimastr61@gmail.com>
2022-06-04 08:35:08 +0300

    добавил сервис Transactional для метода save() при регистрации клиента

--- src/main/java/com/example/myfirstspringbootapplication/service/CustomerService.java
+++ src/main/java/com/example/myfirstspringbootapplication/service/CustomerService.java
@@ -4,10 +4,17 @@ import java.util.List;
 
 import com.example.myfirstspringbootapplication.dto.Customer;
 import org.springframework.data.repository.CrudRepository;
+import org.springframework.transaction.annotation.Isolation;
+import org.springframework.transaction.annotation.Propagation;
+import org.springframework.transaction.annotation.Transactional;
 
 public interface CustomerService extends CrudRepository<Customer, Long> {
 
     List<Customer> findByLastName(String lastName);
 
     Customer findById(long id);
+
+    @Override
+    @Transactional(isolation = Isolation.SERIALIZABLE, propagation = Propagation.REQUIRES_NEW)
+    <S extends Customer> S save(S s);
 }
